417eb1fb920f47bc03c025b89e362d40a2cb846d
Baily <yinbah@163.com>
2018-11-22 20:44:00 +0800

    修改common扫描配置

--- src/main/java/com/benny/framework/common/CommonScan.java
+++ src/main/java/com/benny/framework/common/CommonScan.java
@@ -1,8 +1,13 @@
 package com.benny.framework.common;
 
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
 /**
  * @author yin.beibei
  * @date 2018/11/22 17:17
  */
+@Configuration
+@ComponentScan(basePackageClasses = CommonScan.class)
 public class CommonScan {
 }
--- src/main/java/com/benny/framework/common/framework/config/app/DefaultAppConfig.java
+++ src/main/java/com/benny/framework/common/framework/config/app/DefaultAppConfig.java
@@ -1,11 +1,9 @@
 package com.benny.framework.common.framework.config.app;
 
-import com.benny.framework.common.CommonScan;
 import com.benny.framework.common.framework.service.template.CommonServiceExecuteTemplate;
 import com.benny.framework.common.framework.service.template.ServiceExecuteTemplate;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 /**
@@ -14,7 +12,6 @@ import org.springframework.context.annotation.Configuration;
  * @date 2018/11/22 15:42
  */
 @Configuration
-@ComponentScan(basePackageClasses = CommonScan.class)
 public class DefaultAppConfig {
     /**
      * 获取业务执行模板
